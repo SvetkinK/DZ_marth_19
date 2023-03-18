86439c414cde4ca5be284c8e01224d255c6399c6
Svetlana <khabarova09@rambler.ru>
2023-03-18 16:11:22 +0300

    Последовательно все числа в кубе

---
+++ InNumberOutAllNumbersInCube/Program.cs
@@ -0,0 +1,14 @@
+﻿// На вход число N, на выход таблица кубов чисео от 1 до N
+void NumbersInCube(int n)
+{
+  int b = 0;
+  for (int i = 1; i <= n; i++)
+  {
+    b = i * i * i;
+    Console.Write($"{b}, ");
+  }
+}
+
+Console.Write("введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+NumbersInCube(n);
