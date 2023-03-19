c321869fc19e026a34810f2df3af475e8967b90a
Svetlana <khabarova09@rambler.ru>
2023-03-19 22:17:16 +0300

    Расстояние в космосе

---
+++ DistanceInSpace/Program.cs
@@ -0,0 +1,27 @@
+﻿// На вход координаты двух точек, на выход расстояние между ними в n-мерном пространстве
+//Сначала задать n, затем задать координаты точек
+
+Console.Write("Введите мерность пространства ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] point1 = new int [n];
+for(int index = 0; index < n; index++)
+{
+  Console.Write("Введите координаты точки 1 ");
+  point1[index] = Convert.ToInt32(Console.ReadLine());
+}
+
+int [] point2 = new int [n];
+for(int index = 0; index < n; index++)
+{
+  Console.Write("Введите координаты точки 2 ");
+  point2[index] = Convert.ToInt32(Console.ReadLine());
+}
+
+double dist = 0;
+for(int index = 0; index < n; index++)
+{
+  dist = dist + (point1[index]-point2[index])*(point1[index]-point2[index]);
+}
+dist = Math.Sqrt(dist);
+Console.WriteLine(dist);
